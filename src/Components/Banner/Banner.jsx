f919769295065184f42961dd81720dd9cdb55e46
Sajid5659 <muhammadshajedulislam@gmail.com>
2025-10-28 01:57:20 +0600

    fixed background image

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,17 +4,17 @@ const Banner = () => {
     return (
         <div>
             <div
-            className="hero justify-start items-center"
+            className="hero justify-start items-center min-h-screen"
             style={{
                 backgroundImage:
                 "url(https://i.ibb.co.com/LHJdSx3/Banner-min.jpg)",
             }}
             >
-            <div className="hero-overlay"></div>
-            <div className="hero-content  text-neutral-content">
-                <div className="max-w-md p-10">
-                <h1 className="mb-5 text-3xl font-bold">Bid on Unique Items from Around the World</h1>
-                <p className="mb-5">
+            <div className="hero "></div>
+            <div className="hero-content  text-neutral-content max-w-lg">
+                <div className=" p-10">
+                <h1 className="mb-5 text-4xl font-bold">Bid on Unique Items from Around the World</h1>
+                <p className="mb-5 text-xl">
                     Discover rare collectibles, luxury goods, and vintage treasures in our curated auctions.
                 </p>
                 <button className="btn btn-primary">Explore Auctions</button>
